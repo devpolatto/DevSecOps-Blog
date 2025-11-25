fee4a608a53dd3bcf3f21113884c09f570ab6c6c
devpolatto <polatto.xcode.job@gmail.com>
2025-11-25 10:23:24 -0300

    Quartz sync: Nov 25, 2025, 10:23 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "pt-BR",
-    baseUrl: "blog-devsecops.polatto.dev.br",
+    baseUrl: "devpolatto.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
